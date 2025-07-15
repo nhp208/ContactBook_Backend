313cd4ae89af73066f5267f1f4a8594cf71d5c07
nhp208 <nhp30028002@gmail.com>
2025-07-15 18:38:22 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const ApiError = require("./app/api-error");
+
 const app = express();
 const contactsRouter = require("./app/routes/contact.route");
 app.use(cors());
@@ -8,4 +10,18 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to contact book application." });
 });
 app.use("/api/contacts", contactsRouter);
+//handle 404 response
+app.use((req, res, next) => {
+  //code ở đây sẽ chạy khi không có route được định nghĩa
+  //nếu khớp với yêu cầu. Gọi next() để chuyển sang middleware xử lý lỗi
+  return next(new ApiError(404, "Resource not fount"));
+});
+
+app.use((error, req, res, next) => {
+  //middleware xử lý lỗi tập trung
+  //trong các đoạn code xử lý ở các route, gọi next(error) se chuyển về middleware lỗi này
+  return res.status(error.statusCode || 500).json({
+    message: error.message || "Internal Server Error",
+  });
+});
 module.exports = app;
